5b69dea908ac3a564e64d8fce672960a4cccaf71
motiwalam <76634416+motiwalam@users.noreply.github.com>
2025-09-29 00:24:22 +0000

    Deploying to main from @ motiwalam/piazza-revert@9f5d542e4ff338800476bc3a32b52df57a902223 🚀

---
+++ revert.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name Piazza Revert
+// @description Revert Piazza to the old user interface
+// @version 0.5.0-build.2025-09-29T00:24:19.470Z
+// @author embeddedt
+// @homepage https://github.com/embeddedt/piazza-revert
+// @supportURL https://github.com/embeddedt/piazza-revert/issues
+// @match https://piazza.com/*
+// @connect piazza.com
+// @downloadURL https://github.com/embeddedt/piazza-revert/raw/refs/heads/main/revert.user.js
+// @grant GM_addStyle
+// @grant GM_addElement
+// @grant GM_xmlhttpRequest
+// @icon https://www.google.com/s2/favicons?sz=64&domain=piazza.com
+// @namespace https://github.com/embeddedt
+// @updateURL https://github.com/embeddedt/piazza-revert/raw/refs/heads/main/revert.meta.js
+// ==/UserScript==
